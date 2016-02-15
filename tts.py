44f9e8aa48d89d373109b90a4fd4cbe57a475e67
facundoq <facundoq@gmail.com>
2016-02-15 18:47:53 -0300

    added all diphones, but they are not segmented correctly. Refactored wav concatenation in tts.py

--- tts.py
+++ tts.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 import sys
 import os
-# input: mamAsalAlapApa
-# outpute: -m,ma,am,Ma,As,sa,
 
-vowels=['a','A']
-consonants=['k','l','m','s','p',]
 
-diphones_path="diphones"
 
+
+
+
+
+# checks the argument count and the syntax of the input string to synthesize
 def validate_arguments():
     argument_count=len(sys.argv)
     if ( argument_count != 3 ):
@@ -20,7 +20,11 @@ def validate_arguments():
         sys.exit('Invalid input string syntax.')
     return input_string,output_filepath
 
+# validates the syntax of the input string to check it only contains
+# Consonant-Vowel syllables
 def validate_syntax(input_string):
+    vowels=['a','A']
+    consonants=['k','l','m','s','p']
     n=len(input_string)
     if n == 0:
         return False
@@ -34,6 +38,9 @@ def validate_syntax(input_string):
             return False
     return True
 
+# receives an input string with valid syntax
+# returns the list of diphones the string represents
+# as a list of strings
 def tokenize(input_string):
     n=len(input_string)
     pairs= n / 2
@@ -44,33 +51,46 @@ def tokenize(input_string):
     diphones.append(input_string[-1])
     return diphones
 
+# concatenates two wav files into another
 def concat2_wav(wav_filepath1,wav_filepath2,output_filepath):
     pid=os.getpid()
     #print 'sox %s -r 44100  /tmp/%d-1.raw' % (wav_filepath1,pid)
-    os.system('sox %s -r 44100  /tmp/%d-1.raw' % (wav_filepath1,pid))
-    os.system('sox %s -r 44100 /tmp/%d-2.raw' % (wav_filepath2,pid))
-    os.system('cat /tmp/%d-1.raw /tmp/%d-2.raw > /tmp/%d.raw' % (pid,pid,pid))
-    os.system('sox -r 44100 -e signed-integer -b 16 -c 1 /tmp/%d.raw %s' % (pid,output_filepath))
-    os.system('rm /tmp/%d*.raw' % pid)
+    options = "-r 16000 -b 16 -c 1 -e signed"
+    options_input=options
+    temp1="/tmp/%d-1.raw" % pid
+    temp2="/tmp/%d-2.raw" % pid
+    temp_output="/tmp/%d_.raw" % pid
+    os.system('sox %s %s %s' % (wav_filepath1,options_input,temp1))
+    os.system('sox %s %s %s' % (wav_filepath2,options_input,temp2))
+    os.system('cat %s %s > %s' % (temp1,temp2,temp_output))
+    os.system('sox %s %s %s %s' % (options,temp_output,options,output_filepath))
+    os.system('rm %s' % temp_output)
 
-def concat_wav(diphone_filepaths,output_filepath):
-    concat2_wav(diphone_filepaths[0],diphone_filepaths[1],output_filepath)
-    for i in xrange(2,len(diphone_filepaths)):
-        concat2_wav(output_filepath,diphone_filepaths[i],output_filepath)
+# concatenate multiple wavs into a single one
+# by repeatedly concatenating two wavs
+# list of wavs must contain at least two files
+def concat_wav(wav_filepaths,output_filepath):
+    concat2_wav(wav_filepaths[0],wav_filepaths[1],output_filepath)
+    for i in xrange(2,len(wav_filepaths)):
+        concat2_wav(output_filepath,wav_filepaths[i],output_filepath)
 
-
-def synthesize(diphones,output_filepath):
+# takes a list of diphones and outputs a wav file with the wavs of each
+# diphone (taken from the diphones_path) concatenated
+def synthesize(diphones_path,diphones,output_filepath):
     diphone_filenames=map(lambda diphone:  diphone+".wav",diphones)
     diphone_filepaths=map(lambda filename: os.path.join(diphones_path,filename),diphone_filenames)
     concat_wav(diphone_filepaths,output_filepath)
 
+# synthesizes a string to speech. the string is received in sys.argv
 def main():
+    diphones_path="diphones"
+
     input_string,output_filepath=validate_arguments()
     print "Synthesizing wav for input: %s" % input_string
     diphones=tokenize(input_string)
     print "Synthesizing diphones: %s" % str(diphones)
     print "..."
-    synthesize(diphones,output_filepath)
+    synthesize(diphones_path,diphones,output_filepath)
     print "Synthesis complete."
 
 
@@ -79,9 +99,10 @@ if __name__ == "__main__":
     main()
 
 
-# #!/bin/sh
-# sox $1 -r 44100 -c 2 -s -w /tmp/$$-1.raw
-# sox $2 -r 44100 -c 2 -s -w /tmp/$$-2.raw
-# cat /tmp/$$-1.raw /tmp/$$-2.raw > /tmp/$$.raw
-# sox -r 44100 -c 2 -s -w /tmp/$$.raw $3
-# rm /tmp/$$*.raw
+
+# -k, -l, -m, -s, -p
+# ka, la, ma, sa, pa
+# ak, al, am, as, ap
+# kA, lA, mA, sA, pA
+# Ak, Al, Am, As, Ap
+# a-, A-
